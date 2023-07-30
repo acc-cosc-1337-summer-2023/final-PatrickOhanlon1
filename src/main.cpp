38231c0492bbe5b6e0605c8981f99d11d3b070a3
PatrickOhanlon1 <135080049+PatrickOhanlon1@users.noreply.github.com>
2023-07-30 20:33:05 +0000

    Question 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
+#include "catch.hpp"
+#include "die.h"
+#include "roll.h"
+#include "phase.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+
 int main() 
 {
-	
+	srand(time(0));
 	return 0;
 }
+
+void Roll::roll_die() 
+{
+    die1.roll();
+    die2.roll();
+	int *roll = Roll;
+	roll = throw_die;
+
+}
+
+class Shooter
+{
+	while(come_out_phase(get_outcome RollOutcome::natural) || (RollOutcome::craps) )
+	{
+		return value;
+		cout << value;
+		roll = throw_die;
+	}
+
+	while(point_phase(get_outcome RollOutcome::point) || (RollOutcome::seven_out) )
+	{
+		return value;
+		cout << value;
+		roll = throw_die;
+	}
+};
